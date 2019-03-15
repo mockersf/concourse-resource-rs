757b76608e00bbd135cc51d90454faf548c177ca
François Mockers <mockersf@gmail.com>
2019-03-15 19:34:07 +0100

    :memo: doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,161 @@
-/// https://concourse-ci.org/implementing-resource-types.html
+#![deny(
+    warnings,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications,
+    missing_docs
+)]
+
+//! Helper to implement a [Concourse](https://concourse-ci.org/) resource in Rust
+//!
+//! [Concourse documentation](https://concourse-ci.org/implementing-resource-types.html)
+
 use std::fmt::Debug;
 
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
+/// Output of the "in" step of the resource
 #[derive(Serialize, Deserialize, Debug)]
 pub struct InOutput<V, M> {
+    /// The fetched version.
     pub version: V,
+    /// A list of key-value pairs. This data is intended for public consumption and will make
+    /// it upstream, intended to be shown on the build's page.
     pub metadata: Option<M>,
 }
 
-#[derive(Serialize, Deserialize)]
+/// Output of the "out" step of the resource
+#[derive(Serialize, Deserialize, Debug)]
 pub struct OutOutput<V, M> {
+    /// The resulting version.
     pub version: V,
+    /// A list of key-value pairs. This data is intended for public consumption and will make
+    /// it upstream, intended to be shown on the build's page.
     pub metadata: Option<M>,
 }
 
+/// Input of the "check" step of the resource
 #[derive(Deserialize, Debug)]
 pub struct CheckInput<S, V> {
+    /// Resource configuration, from the `source` field
     pub source: S,
+    /// Latest version retrieved, or `None` on first check
     pub version: Option<V>,
 }
 
+/// Input of the "in" step of the resource
 #[derive(Deserialize, Debug)]
 pub struct InInput<S, V, P> {
+    /// Resource configuration, from the `source` field
     pub source: S,
+    /// Version to retrieve
     pub version: V,
+    /// Step configuration, from the `params` field
     pub params: Option<P>,
 }
 
+/// Input of the "out" step of the resource
 #[derive(Deserialize, Debug)]
 pub struct OutInput<S, P> {
+    /// Resource configuration, from the `source` field
     pub source: S,
+    /// Step configuration, from the `params` field
     pub params: Option<P>,
 }
 
-pub struct BuildParameters {
+/// When used in a "get" or "put" step, metadata about the running build is made available
+/// via environment variables.
+///
+/// If the build is a one-off, `build_name`, `build_job_name`, and `build_pipeline_name`
+/// will be `None`.
+///
+/// [Concourse documentation](https://concourse-ci.org/implementing-resource-types.html#resource-metadata)
+#[derive(Debug)]
+pub struct BuildMetadata {
+    /// The internal identifier for the build. Right now this is numeric but it may become
+    /// a guid in the future. Treat it as an absolute reference to the build.
     pub build_id: String,
+    /// The build number within the build's job.
     pub build_name: Option<String>,
+    /// The name of the build's job.
     pub build_job_name: Option<String>,
+    /// The pipeline that the build's job lives in.
     pub build_pipeline_name: Option<String>,
+    /// The team that the build belongs to.
     pub build_team_name: String,
+    /// The public URL for your ATC; useful for debugging.
     pub atc_external_url: String,
 }
 
+/// The methods and associated types needed to implement a resource
 pub trait Resource {
+    /// Resource configuration, from the `source` field
     type Source: DeserializeOwned + Debug;
+    /// A version of the resource
     type Version: Serialize + DeserializeOwned + Debug;
 
+    /// Parameters for the "in" step, from the `params` field
     type InParams: DeserializeOwned + Debug;
+    /// A list of key-value pairs for the "in" step. This data is intended for public
+    /// consumption and will make it upstream, intended to be shown on the build's page.
     type InMetadata: Serialize + Debug;
+    /// Parameters for the "out" step, from the `params` field
     type OutParams: DeserializeOwned + Debug;
+    /// A list of key-value pairs for the "out" step. This data is intended for public
+    /// consumption and will make it upstream, intended to be shown on the build's page.
     type OutMetadata: Serialize + Debug;
 
-    /// https://concourse-ci.org/implementing-resource-types.html#resource-check
+    /// A resource type's check method is invoked to detect new versions of the resource. It is
+    /// given the configured source and current version, and must return the array of new
+    /// versions, in chronological order, including the requested version if it's still valid.
+    ///
+    /// [Concourse documentation](https://concourse-ci.org/implementing-resource-types.html#resource-check)
     fn resource_check(source: Self::Source, version: Option<Self::Version>) -> Vec<Self::Version>;
 
-    /// https://concourse-ci.org/implementing-resource-types.html#in
+    /// The in method is passed the configured source, a precise version of the resource to fetch
+    /// and a destination directory. The method must fetch the resource and place it in the given
+    /// directory.
+    ///
+    /// If the desired resource version is unavailable (for example, if it was deleted), the
+    /// method must return an error.
+    ///
+    /// The method must return the fetched version, and may return metadata as a list of
+    /// key-value pairs. This data is intended for public consumption and will make it upstream,
+    /// intended to be shown on the build's page.
+    ///
+    /// [Concourse documentation](https://concourse-ci.org/implementing-resource-types.html#in)
     fn resource_in(
         source: Self::Source,
         version: Self::Version,
         params: Option<Self::InParams>,
-        path: &str,
-    ) -> InOutput<Self::Version, Self::InMetadata>;
+        output_path: &str,
+    ) -> Result<InOutput<Self::Version, Self::InMetadata>, Box<std::error::Error>>;
 
-    /// https://concourse-ci.org/implementing-resource-types.html#out
+    /// The out method is called with the resource's source configuration, the configured params
+    /// and a path to the directory containing the build's full set of sources.
+    ///
+    /// The script must return the resulting version of the resource. Additionally, it may return
+    /// metadata as a list of key-value pairs. This data is intended for public consumption and
+    /// will make it upstream, intended to be shown on the build's page.
+    ///
+    /// [Concourse documentation](https://concourse-ci.org/implementing-resource-types.html#out)
     fn resource_out(
         source: Self::Source,
         params: Option<Self::OutParams>,
+        input_path: &str,
     ) -> OutOutput<Self::Version, Self::OutMetadata>;
 
-    /// https://concourse-ci.org/implementing-resource-types.html#resource-metadata
-    fn build_metadata() -> BuildParameters {
-        BuildParameters {
+    /// When used in a "get" or "put" step, will return [metadata](struct.BuildMetadata.html) about the running build is
+    /// made available via environment variables.
+    ///
+    /// [Concourse documentation](https://concourse-ci.org/implementing-resource-types.html#resource-metadata)
+    fn build_metadata() -> BuildMetadata {
+        BuildMetadata {
             build_id: std::env::var("BUILD_ID")
                 .expect("environment variable BUILD_ID should be present"),
             build_name: std::env::var("BUILD_NAME").ok(),
@@ -85,6 +169,7 @@ pub trait Resource {
     }
 }
 
+/// Macro that will build the `main` function from a struct implementing the `Resource` trait
 #[macro_export]
 macro_rules! create_resource {
     ($resource:ty) => {
@@ -109,10 +194,7 @@ macro_rules! create_resource {
                         .expect("error deserializing input");
                     let result =
                         <$resource as Resource>::resource_check(input.source, input.version);
-                    println!(
-                        "{}",
-                        serde_json::to_string(&result).expect("error serializing response")
-                    );
+                    dbg!(serde_json::to_string(&result)).expect("error serializing response");
                 }
                 "/opt/resource/in" => {
                     let input: InInput<
@@ -129,10 +211,14 @@ macro_rules! create_resource {
                             .next()
                             .expect("expected path as first parameter"),
                     );
-                    println!(
-                        "{}",
-                        serde_json::to_string(&result).expect("error serializing response")
-                    );
+                    match result {
+                        Err(error) => {
+                            eprintln!("Error! {}", error);
+                            std::process::exit(1);
+                        }
+                        Ok(result) => dbg!(serde_json::to_string(&result))
+                            .expect("error serializing response"),
+                    };
                 }
                 "/opt/resource/out" => {
                     let input: OutInput<
@@ -140,11 +226,14 @@ macro_rules! create_resource {
                         <$resource as Resource>::OutParams,
                     > = dbg!(serde_json::from_str(&input_buffer))
                         .expect("error deserializing input");
-                    let result = <$resource as Resource>::resource_out(input.source, input.params);
-                    println!(
-                        "{}",
-                        serde_json::to_string(&result).expect("error serializing response")
+                    let result = <$resource as Resource>::resource_out(
+                        input.source,
+                        input.params,
+                        &std::env::args()
+                            .next()
+                            .expect("expected path as first parameter"),
                     );
+                    dbg!(serde_json::to_string(&result)).expect("error serializing response");
                 }
                 v => eprintln!("unexpected being called as '{}'", v),
             }
